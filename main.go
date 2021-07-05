fe2bb451b92a744f2d61ebd40d5a3e2184df4d06
Prince Khanna <prince.khanna@atoss.com>
2021-07-05 16:10:33 +0300

    first commit

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	ent "internship/golang/entity"
+)
+
+func main() {
+	var input string
+	fmt.Scanln(&input)
+	fmt.Println(input)
+	var names = []string{"a", "b", "c", "d", "e"}
+	//fmt.Println(names)
+	person := ent.Person{Name: "Prince", Gender: "Male"}
+	fmt.Println(person.IsMale())
+	for i, name := range names {
+		fmt.Println(name, "At Postition", i)
+	}
+}
+
+func ReturnTypes() (name string, age int) {
+	return "name", 23
+}
